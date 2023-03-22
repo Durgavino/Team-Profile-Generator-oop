63269e1c761ca1acca13f6fef6e9ce5f16283412
Durgavino <mailtodurga93@gmail.com>
2023-03-22 15:51:57 -0300

    testing the employee nameid email only

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,34 +1,36 @@
 const Employee = require('../lib/Employee');
 
-test("Employee", () => {
-    test("Initialization", () => {
+//test("Employee", () => {
+    // test("Initialization", () => {
         it("Should Create an Object with name,id,Email", () => {
             const name = new Employee("Durga", 1, "durga@gmail.com");
             expect(Employee.name).toEqual("Durga");
             expect(Employee.id).toEqual(1);
             expect(Employee.email).toEqual("durga@gmail.com");
         });
-        it("Should throw an error if there is no arguments", () => {
+//         it("Should throw an error if there is no arguments", () => {
 
-            const cb = () => new Employee();
-            expect(cb).toThrowError();
-        });
-        it('Should throw an error if not provided a name', () => {
-            const cb = () => new Employee(1,1,"durga@gmail.com");
-            const err = new Error("Expected Parameter 'name to be a string");
-            expect(cb).toThrowError(err);
-        });
-        it('Should throw an error if id is less than 0',()=>{
-            const cb=()=>new Employee("Durga",-1,"durga@gmail.com");
-            const err=new Error('Expected Parameter "id" to be non negative number');
-            expect (cb).toThrowError(err);
-        });
-        it('Should throw an error if "Email" is without @',()=>{
-            const cb=()=>new Employee("Durga",-1,"durgagmail.com");
-            const err=new Error('Expected Paramater "Email" to be with @');
-            expect(cb).toThrowError(err);
+//             const cb = () => new Employee();
+//             expect(cb).toThrowError();
+//         });
+//    // });
 
-        });
+//         it('Should throw an error if not provided a name', () => {
+//             const cb = () => new Employee(1,1,"durga@gmail.com");
+//             const err = new Error("Expected Parameter 'name to be a string");
+//             expect(cb).toThrowError(err);
+//         });
+//         it('Should throw an error if id is less than 0',()=>{
+//             const cb=()=>new Employee("Durga",-1,"durga@gmail.com");
+//             const err=new Error('Expected Parameter "id" to be non negative number');
+//             expect (cb).toThrowError(err);
+//         });
+//         it('Should throw an error if "Email" is without @',()=>{
+//             const cb=()=>new Employee("Durga",-1,"durgagmail.com");
+//             const err=new Error('Expected Paramater "Email" to be with @');
+//             expect(cb).toThrowError(err);
+
+//         });
 
         
 
@@ -39,5 +41,5 @@ test("Employee", () => {
 
 
 
-    });
-});
+//     });
+// });
