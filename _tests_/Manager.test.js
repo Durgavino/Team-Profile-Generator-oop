a01f9e86619eb419d3d4af545efe806383a39aec
Durgavino <mailtodurga93@gmail.com>
2023-03-24 11:54:22 -0300

    added officenumber manager for getrole method

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -8,3 +8,19 @@ test("Should create an Object with officeNumber",()=>{
     const manager=new Manager(name, id, email,officeNumber);
     expect(manager.officeNumber).toBe(officeNumber);
 });
+test("testing a getGithub for Engineer",()=>{
+    const name = "Durga";
+    const id = 1;
+    const email = "durga@gmail.com";
+    const officeNumber=123456789;
+    const manager=new Manager(name, id, email,officeNumber);
+    expect(manager.getOfficeNumber()).toBe(officeNumber);
+});
+test("testing a getRole for Manager",()=>{
+    const name = "Durga";
+    const id = 1;
+    const email = "durga@gmail.com";
+    const officeNumber=123456789;
+    const manager=new Manager(name, id, email,officeNumber);
+    expect(manager.getRole()).toBe("Manager");
+});
