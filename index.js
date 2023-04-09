b74a6a7bae5e7c34124e63ed8a1be7829df48d6a
Durgavino <mailtodurga93@gmail.com>
2023-04-08 22:50:13 -0300

    adding employee type

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
-const { default: Choices } = require('inquirer/lib/objects/choices');
+
+const Manager = require('./lib/Manager');
 
 
-const questions = [{
-    type: 'input',
-    message: 'what is you Team Manager Name',
-    name: 'managerName'
-},
-{
-    type: 'input',
-    message: 'what is you Team Manager Email',
-    name: 'managerEmail'
-},
-{
-    type: 'input',
-    message: 'what is you Team Manager Office Number',
-    name: 'managerNumber'
-},
-{
-    type: 'input',
-    message: 'what is you Team Manager ID',
-    name: 'managerId'
-},
-{
-    type: 'list',
-    message: 'Which type of Team Member you would like to add?',
-    name: 'manager',
-    Choices: ["Engineer","Intern","I dont want to add the members"]
-}]
+function employeeType() {
+    inquirer.prompt(
+        [{
+            type: 'list',
+            message: 'Select the Employee Type :',
+            name: 'employee',
+            choices: ["Manager", "Engineer", "Intern"]
+        }]
+    ).then(data => {
+        console.log(data);
+    }
+    
+  
+)}
+
+employeeType();
