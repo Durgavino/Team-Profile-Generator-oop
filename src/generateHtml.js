5e5a7997c7eabca0eb1dc79a37c21c3d6592e864
Durgavino <mailtodurga93@gmail.com>
2023-04-10 15:19:36 -0300

    added Manager details

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,32 +1,30 @@
-const generateManager= function(manager){
-    return`${manager.name}${manager.id}${manager.email}${manager.officeNumber}`;
+const generateManager = function (manager) {
+    return `${manager.name}${manager.id}${manager.email}${manager.officeNumber}`;
 }
 
-const generateEngineer= function(engineer){
-    return`${engineer.name}${engineer.id}${engineer.email}${engineer.github}`;
+const generateEngineer = function (engineer) {
+    return `${engineer.name}${engineer.id}${engineer.email}${engineer.github}`;
 }
 
-const generateIntern= function(intern){
-    return`${intern.name}${intern.id}${intern.email}${intern.school}`;
+const generateIntern = function (intern) {
+    return `${intern.name}${intern.id}${intern.email}${intern.school}`;
 }
 
 
-function employeePicker(){
-
-    if(teammember[i].role == "Manager"){
-        starter += generateManager(teammember[i])
-
-        }else if(teammember[i].role == "Engineer"){
-            // same as above but for the generateIntern 
+function employeePicker() {
+    const teammember = [];
+    for (let i = 0; i < teammember.length; i++) {
+        if (teammember[i].role == "Manager") {
+            starter += generateManager(teammember[i])
+        } else if (teammember[i].role == "Engineer") {
             starter += generateEngineer(teammember[i])
-        }else if(teammember[i].role == "Intern"){
-            // same as above but for the generateengineer 
+        } else if (teammember[i].role == "Intern") {
             starter += generateIntern(teammember[i])
         }
-
+    }
 }
 
-const generateHTML = (teammember)=>{
+const generateHTML = teammember => {
     console.log(teammember);
     return `
     <!DOCTYPE html>
@@ -38,7 +36,11 @@ const generateHTML = (teammember)=>{
     <title>Document</title>
 </head>
 <body>
-    ${employeePicker()}
+    // //${employeePicker()}
+  MANAGER NAME:  ${teammember[0].name},
+  Manager Email: ${teammember[0].id},
+  MANAGER ID: ${teammember[0].email},
+  Manager OfficeNumber:${teammember[0].officeNumber},
 </body>
 </html>
 
@@ -47,11 +49,11 @@ const generateHTML = (teammember)=>{
 //     var starter = `` // this is going to be the final string that we will append to 
 //     // for loop of each member
 //     for( let i=0;i<teammember.length;i++){
-    
+
 
 //            }
 //         return starter;
 // }
 
 
-module.exports=generateHTML;
+module.exports = generateHTML;
